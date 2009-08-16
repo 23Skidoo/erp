1d2cefd2379bf63ea758c24f388fc9db072af0d1
Mikhail Glushenkov <c05mgv@cs.umu.se>
2009-08-16 23:12:58 +0200

    Add inference tests.

--- Test.hs
+++ Test.hs
@@ -24,3 +24,33 @@ testConstraints4 = [(STBase "Y", STFun STInt (STBase "Y"))]
 -- [].
 testConstraints5 :: ConstraintSet
 testConstraints5 = []
+
+-- x_1 -> int; x_0 -> x_1.
+testConstraints6 :: ConstraintSet
+testConstraints6 = [(STFun (STBase "x_0") (STBase "x_0"),
+                     STFun (STBase "x_1") STInt)]
+
+-- x_1 -> int; x_0 -> int.
+testConstraints7 :: ConstraintSet
+testConstraints7 = [(STFun (STBase "x_0") (STBase "x_0"),
+                     STFun STInt (STBase "x_1"))]
+
+-- xfail.
+testConstraints8 :: ConstraintSet
+testConstraints8 = [(STStr,STInt),(STInt,STInt)]
+
+-- int.
+testInference1 :: AST
+testInference1 = (app (lambda (var "x") (var "x")) (int 1))
+
+-- x -> x.
+testInference2 :: AST
+testInference2 = (lambda (var "x") (var "x"))
+
+-- int -> int
+testInference3 :: AST
+testInference3 = (lambda (var "x") (plus (var "x") (var "x")))
+
+-- xfail
+testInference4 :: AST
+testInference4 = (plus (str "abc") (int 2))
